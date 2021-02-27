b21121359373547768fcb5ed511cef66dd6bc0a8
Кирюшин Евгений Андреевич <ZhenikaKi@gmail.com>
2021-02-27 12:23:09 +0500

    Домашнее задание

--- app/src/main/java/ru/kirea/androidplatform/MainActivity.java
+++ app/src/main/java/ru/kirea/androidplatform/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lesson1_1); //1 4 задание (ems - задает ширину поля, равной ширине самой широкой буквы в шрифте * число ems)
+        //setContentView(R.layout.activity_lesson1_2_and_3); //2 и 3 задание
     }
 }
